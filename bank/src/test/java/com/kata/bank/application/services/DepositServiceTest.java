78a2c578267555cba052ec22dc5982f49aa641ea
moham <moham@Desktop>
2022-03-20 16:54:33 +0100

    :test red

---
+++ bank/src/test/java/com/kata/bank/application/services/DepositServiceTest.java
@@ -0,0 +1,13 @@
+package com.kata.bank.application.services;
+
+import org.junit.jupiter.api.Test;
+
+public class DepositServiceTest {
+
+	private DepositService service;
+	@Test
+	void shouldDepositSuccesfully() {
+		
+	}
+	
+}
